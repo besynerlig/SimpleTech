779dc2c92e62492ec710a2876ae245bd7f359079
Exa <dennismatsson99@gmail.com>
2018-05-30 13:38:19 +0200

    Working login
    The login is now working and commented.

--- accounts/login.php
+++ accounts/login.php
@@ -1,33 +1,27 @@
 <?php require "../db.php";
 
+  //declare variables
   $username = $password = "";
 
+  //grab the info from the form
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = mysqli_real_escape_string ( $con,  $_POST['username']);
-    $password = $_POST['password'];
+    $username = mysqli_real_escape_string ( $con, $_POST['username']);
+    $password = mysqli_real_escape_string ( $con, $_POST['password']);
 
-    // $hash = $con->query("SELECT password FROM users WHERE username = '$username'")->fetch_object()->password;
-    //
-    // print_r($password);?><br><?php
-    //
-    // $hash2 = password_hash($password, PASSWORD_DEFAULT);
-    // echo $hash2;?><br><?php
-    //
-    // $hash3 = password_hash($password, PASSWORD_DEFAULT);
-    // echo $hash3;?><br><?php
-    //
-    // print_r($hash);?><br><?php
-    //
-    // var_dump(password_verify($password, $hash));
+    //grab the hash from the database
+    $hash = $con->query("SELECT password FROM users WHERE username = '$username'")->fetch_object()->password;
 
+    //verify the password
     if (password_verify($password, $hash)) {
+      //if true, log them in, send them to select deparatment.
       session_start();
       $_SESSION['username'] = $username;
       header("Location: ../seldep.php");
       die();
     }
     else {
-      header("Location: ../index.php?1");
+      //if false, send them back to index with error code
+      header("Location: ../index.php?err");
       die();
     }
   }
--- accounts/registerdone.php
+++ accounts/registerdone.php
@@ -2,28 +2,35 @@
 <?php session_start(); ?>
 
 <?php
-
+//declare the variables
 $username = $email = $password = $password2 = "";
 
+//grab the info from the form
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = mysqli_real_escape_string ( $con,  $_POST['username']);
   $email = mysqli_real_escape_string ( $con,  $_POST['email']);
-  $password = $_POST['password'];
-  $password2 = $_POST['password2'];
+  $password = mysqli_real_escape_string ( $con,  $_POST['password']);
+  $password2 = mysqli_real_escape_string ( $con,  $_POST['password2']);
 
+  //check if passwords match
   if ($password == $password2) {
-    $hash = password_hash($password2, PASSWORD_DEFAULT);
+    //hash it
+    $hash = password_hash($password, PASSWORD_DEFAULT);
 
+    //create the sql querry
     $sql = "INSERT INTO users (username, email, password)
     VALUES ('$username', '$email', '$hash')";
 
+    //insert it into the database
     if ($con->query($sql) === TRUE) {
+      //if it worked, send them back to register.php with the GET code rg (registered)
       header("Location: register.php?rg");
       die();
       } else {
           echo "Error: " . $sql . "<br>" . $con->error;
       }
     } else {
+      //if it dosent work, sent them back with error code that they dont match
       header("Location: register.php?passwordDontMatch");
     }
   }
